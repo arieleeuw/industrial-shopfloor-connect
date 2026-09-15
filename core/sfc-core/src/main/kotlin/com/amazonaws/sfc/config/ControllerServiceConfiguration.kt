6b22af4a2da7dcd5410c76efb0ad8b51b448cbfa
arieleeuw <aleeuwesteyn@hotmail.com>
2026-09-15 12:32:12 +0200

    Fix validateChannel method to use channelID

--- core/sfc-core/src/main/kotlin/com/amazonaws/sfc/config/ControllerServiceConfiguration.kt
+++ core/sfc-core/src/main/kotlin/com/amazonaws/sfc/config/ControllerServiceConfiguration.kt
@@ -220,7 +220,7 @@ class ControllerServiceConfiguration : ServiceConfiguration() {
     private fun validateChannel(sourceID: String, channelID: String, channel: ChannelConfiguration) {
 
         validateChannelName(sourceID, channel)
-        validateChannelTransformation(sourceID, channelID, channel)
+        validateChannelTransformation(sourceID, channelID, channelID)
         validateChannelValueFilter(sourceID, channelID, channel)
         validateChannelChangeFilter(sourceID, channelID, channel)
         validateChannelConditionFilter(sourceID, channelID, channel)
@@ -285,10 +285,11 @@ class ControllerServiceConfiguration : ServiceConfiguration() {
         )
     }
 
-    private fun validateChannelName(sourceID: String, channel: ChannelConfiguration) {
+
+    private fun validateChannelName(sourceID: String, channelID: String) {
         ConfigurationException.check(
-            ChannelConfiguration.CHANNEL_SEPARATOR !in sourceID,
-            "ChannelID \"$channel\" of source \"$sourceID\" can not contain '${ChannelConfiguration.CHANNEL_SEPARATOR}' character",
+            ChannelConfiguration.CHANNEL_SEPARATOR !in channelID,
+            "ChannelID \"$channelID\" of source \"$sourceID\" can not contain '${ChannelConfiguration.CHANNEL_SEPARATOR}' character",
             "Channels",
             sources[sourceID]?.channels
         )
